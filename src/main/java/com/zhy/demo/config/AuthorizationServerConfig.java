85b67dcc182e951550c79b1e47f27c811e74beb8
zhy <1246230855@qq.com>
2020-06-16 19:12:54 +0800

    update

--- src/main/java/com/zhy/demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/zhy/demo/config/AuthorizationServerConfig.java
@@ -5,13 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -27,6 +34,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Qualifier("dataSource")
     @Autowired
     DataSource dataSource;
+
+    @Autowired
+    private AuthorizationCodeServices authorizationCodeServices;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    ClientDetailsService clientDetailsService;
+
+    @Autowired
+    TokenStore tokenStore;
 
 //内存模式
 //    @Override
@@ -52,14 +70,45 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         return new JdbcClientDetailsService(dataSource);
     }
 
-    //配置客户端信息从数据库中取
+    //客户端详情服务 -配置客户端信息从数据库中取
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.withClientDetails(jdbcClientDetails());
     }
 
+    //令牌管理服务
+    @Bean
+    public AuthorizationServerTokenServices tokenService(){
+        DefaultTokenServices service=new DefaultTokenServices();
+        service.setClientDetailsService(clientDetailsService);//客户端信息服务
+        service.setSupportRefreshToken(true);//是否产生刷新令牌
+        service.setTokenStore(tokenStore);//令牌存储策略
+        service.setAccessTokenValiditySeconds(7200); // 令牌默认有效期2小时
+        service.setRefreshTokenValiditySeconds(259200); // 刷新令牌默认有效期3天
+        return service;
+    }
+
+
+    //令牌访问端点
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore());
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .authenticationManager(authenticationManager) //配置密码模式需要
+                .authorizationCodeServices(authorizationCodeServices)//配置授权码模式需要
+                .tokenServices(tokenService())//令牌管理服务
+                .allowedTokenEndpointRequestMethods(HttpMethod.POST)//允许post提交
+                .tokenStore(tokenStore());
+    }
+    //令牌访问端点安全策略
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security){
+        security
+                .tokenKeyAccess("permitAll()")  ///oauth/token_key公开
+                .checkTokenAccess("permitAll()") //校验令牌端口公开/oauth/check_token
+                .allowFormAuthenticationForClients();//表单认证
+    }
+    @Bean
+    public AuthorizationCodeServices authorizationCodeServices() { //设置授权码模式的授权码如何存取，暂时采用内存方式
+        return new InMemoryAuthorizationCodeServices();
     }
 }
